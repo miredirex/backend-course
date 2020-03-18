5821f801466f74f328fb343b1558540d47e53d02
Dan Z <my@area.gq>
2020-03-18 21:16:30 +0300

    Add Task2

---
+++ Task2/PrintArgs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrintArgs
+{
+    static class Program
+    {
+        private const string ArgumentsSeparator = " ";
+
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters were specified!");
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine($"Number of Arguments: {args.Length}");
+            Console.WriteLine($"Arguments: {string.Join(ArgumentsSeparator, args)}");
+        }
+    }
+}
---
+++ Task2/RemoveDuplicates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RemoveDuplicates
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!");
+                Console.WriteLine("Usage: remove_duplicates.exe <input string>");
+                Environment.Exit(1);
+            }
+
+            var checkedCharacters = "";
+            foreach (var ch in args[0])
+            {
+                if (checkedCharacters.Contains(ch)) continue;
+
+                Console.Write(ch);
+                checkedCharacters += ch;
+            }
+        }
+    }
+}
